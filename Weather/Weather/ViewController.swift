3a94bda37dd01a61a0ce5f0d9eed7a07bc22e07a
ArtyomSolovev <80806951+ArtyomSolovev@users.noreply.github.com>
2021-07-19 17:47:33 +0300

    init Weather

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Артем Соловьев on 19.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
